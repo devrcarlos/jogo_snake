71a714617ef14a86e3dcf40a8f24933b3241f9eb
devrcarlos <carlos.stz@gmail.com>
2020-12-26 17:16:19 -0300

    criar comida

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
+let direction = "right";
 
+let snake = [];
 snake[0] = {
     x: 8 * box, 
     y: 8 * box
 }
 
-let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -22,6 +26,11 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -45,6 +54,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
